e952c45e4fb58eb0a8c56f532990a3bc8b7c1ca2
zaynutdin <zaynu987@mail.ru>
2022-03-18 23:48:56 +0300

    Push

--- scripts/build.sh
+++ scripts/build.sh
@@ -30,7 +30,7 @@ echo "CMakeFile.txt will execute with $CMAKE_KEYS options"
 echo "Executing CMakeFile.txt"
 cmake $CMAKE_KEYS -B $BUILD_DIR -S . >> $LOG_FILE || (echo "Error, check $LOG_FILE")
 
-echo "Build targets with make"
-make --directory=$BUILD_DIR >> $LOG_FILE || (echo "Error, check $LOG_FILE")
+#echo "Build targets with make"
+#make --directory=$BUILD_DIR >> $LOG_FILE || (echo "Error, check $LOG_FILE")
 
 echo "REBUILD FINISHED!"
