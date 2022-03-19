58f7c5c50e93d361b61bece9fc8787d79eab5f0c
zaynutdin <zaynu987@mail.ru>
2022-03-19 06:29:39 +0300

    Updated scripts

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,2 +1,1 @@
-g++ -I project tests/tests.cpp project/src/*.c -lgtest -lpthread
-valgrind --tool=memcheck --leak-check=full -s --track-origins=yes ./a.out
+valgrind --tool=memcheck --leak-check=full -s --track-origins=yes ./build/tests/my_first_test
