523060c26a16460779422534c12bb015955ba29b
zaynutdin <zaynu987@mail.ru>
2022-03-19 06:52:40 +0300

    Updated scripts and yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,9 @@ BUILD_DIR="./build"
 
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR && echo "Creating $BUILD_DIR directory"
-
-cmake $CMAKE_KEYS -B $BUILD_DIR -S .
+cd build
+cmake ..
+cmake --build .
+cd ..
 
 echo "REBUILD FINISHED!"
